85d0c07f18216a1b9758fd642970ab50ce717951
ezemartlopez <emartinezlopez@est.frba.utn.edu.ar>
2023-01-02 16:23:12 -0300

    listo enviar contexto a la pagina

--- plantillas/plantillas/views.py
+++ plantillas/plantillas/views.py
@@ -5,3 +5,12 @@ def simple(request):
     # Basico
     # 1ro Informacion de peticion, 2do plantilla y 3ro contexto
     return render(request, 'simple.html', {})
+
+def dinamico(request, name):
+    # Pasando contexto al llamado de la url
+    categorias = ['code','design','marketing']
+    context = {
+        'name':name,
+        'categorias':categorias
+    }
+    return render(request, 'dinamico.html',context)
