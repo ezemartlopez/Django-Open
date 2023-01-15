58468292210e31223ac04cda418a34588c0d61ae
ezemartlopez <emartinezlopez@est.frba.utn.edu.ar>
2023-01-15 14:25:53 -0300

    Widgets con Django finalizado

--- formularios_django/formularios_django/views.py
+++ formularios_django/formularios_django/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import CommentForm
+from .forms import CommentForm,ContactForm
 def form(request):
     comment_form = CommentForm({'name':'Juanjo', 'url':'http://openbootcamp.com', 'comment':'Un comentario'})
     return render(request, 'form.html',{'comment_form':comment_form})
@@ -11,3 +11,6 @@ def goal(request):
     return HttpResponse(request.POST['name'])
     #return render(request, '')
 
+def widget(request):
+    contact_form = ContactForm()
+    return render(request,'widget.html',{'contact_form':contact_form})
