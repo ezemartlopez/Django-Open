402520c3cc9e771363f2a201d73a28780df4c569
ezemartlopez <emartinezlopez@est.frba.utn.edu.ar>
2023-01-16 12:55:03 -0300

    Usando el ModelAdmin

--- ecommerce/stock/admin.py
+++ ecommerce/stock/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Product
+
 # Register your models here.
-admin.site.register(Product)
+
+#Clase de administracion del producto en modo administrador
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','short_description', 'stock',)#elementos de la clase producto que queremos visualizar
+    search_fields = ('name','short_description',)#campos que se utilizaran en la busqueda
+    list_filter = ('name','stock','discount_until',)#campos que se utilizaran como filtros
+    date_hierarchy = 'discount_until' #seguimiento de fechas crea agrupaciones para facilitar la busqueda
+
+#Al producto le paso esta clase, para visualizar la info requerida
+admin.site.register(Product,ProductAdmin)
