e0260146729ab1692cb039d5fe58d663e9725771
ezemartlopez <emartinezlopez@est.frba.utn.edu.ar>
2023-01-17 17:11:49 -0300

    listas y buscador finalizados

--- agenda/contact/views.py
+++ agenda/contact/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contact
 # Create your views here.
+
+def index(req):
+    contacts = Contact.objects.filter(name__contains = req.GET.get('search',''))
+    context ={
+        'contacts':contacts
+    }
+    return render(req, 'contact/index.html',context=context)
