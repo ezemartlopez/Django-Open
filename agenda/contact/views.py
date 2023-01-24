b1a4b69287faa5f09f945ad4e76c102ca836a6b9
Ezequiel Martinez <zenitram.e14@gmail.com>
2023-01-24 13:50:54 -0300

    listo  practica crud contactos parte 1

--- agenda/contact/views.py
+++ agenda/contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Contact
+from .forms import ContactForm
 # Create your views here.
 
 def index(req):
@@ -9,3 +10,17 @@ def index(req):
         'contacts':contacts
     }
     return render(req, 'contact/index.html',context=context)
+
+def view(req, id):
+    contact = Contact.objects.get(id=id)
+    context = {
+        'contact':contact
+    }
+    return render(req,'contact/detail.html',context)
+
+def edit(req,id):
+    if(req.method == 'GET'):
+        contact = Contact.objects.get(id=id)
+        form = ContactForm(instance=contact)
+        context = {'form':form}
+        return render(req,'contact/create.html',context)
