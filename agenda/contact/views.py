f1128fae00f2105c1701ccb916384f4be719c6b0
Ezequiel Martinez <zenitram.e14@gmail.com>
2023-01-24 15:21:03 -0300

    usando sistema de mensajes de django, listo crud contactos parte 2, ya se creo la opcion de crear y editar los contactos.

--- agenda/contact/views.py
+++ agenda/contact/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Contact
 from .forms import ContactForm
+from django.contrib import messages
+
 # Create your views here.
 
 def index(req):
@@ -19,8 +20,29 @@ def view(req, id):
     return render(req,'contact/detail.html',context)
 
 def edit(req,id):
+    contact = Contact.objects.get(id=id)
     if(req.method == 'GET'):
-        contact = Contact.objects.get(id=id)
+        
         form = ContactForm(instance=contact)
-        context = {'form':form}
+        context = {'form':form, 'id':id}
+        return render(req,'contact/edit.html',context)
+    if(req.method == 'POST'):
+        form = ContactForm(req.POST, instance=contact)
+        #vemos si el formulario recibido es valido
+        if(form.is_valid()):
+            form.save()
+        context = {'form':form, 'id':id}
+        #agrego un mensage, y los tags son en este caso las clases que tienen los mensajes(ver como se implementan)
+        messages.success(req,message='Contacto actualizado.', extra_tags='alert-success')
+        return render(req,'contact/edit.html',context)
+
+def create(req):
+    if(req.method =='GET'):
+        form = ContactForm()
+        context = {'form': form}
         return render(req,'contact/create.html',context)
+    if req.method == 'POST':
+        form = ContactForm(req.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('contact')
